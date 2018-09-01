e08f4cb75c0f0802fd0349a42cc2a0fa90945d63
dso <den.soima@gmail.com>
2018-09-01 23:58:24 +0300

    Pagination completed

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -8,7 +8,7 @@ namespace SportsStore.Controllers
     public class ProductController : Controller
     {
         private IProductRepository _repository;
-        public int PageSize = 4;
+        public int PageSize = 2;
 
         public ProductController(IProductRepository repository)
         {
--- Infrastructure/PageLinkTagHelper.cs
+++ Infrastructure/PageLinkTagHelper.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
@@ -7,7 +9,7 @@ using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Infrastructure
 {
-    [HtmlTargetElement("div", Attributes = "page-model")]
+    [HtmlTargetElement("ul", Attributes = "page-model")]
     public class PageLinkTagHelper : TagHelper
     {
         private IUrlHelperFactory _urlHelperFactory;
@@ -17,26 +19,93 @@ namespace SportsStore.Infrastructure
             _urlHelperFactory = urlHelperFactory;
         }
 
-        [ViewContext]
-        [HtmlAttributeNotBound]
-        public ViewContext ViewContext { get; set; }
+        [ViewContext] [HtmlAttributeNotBound] public ViewContext ViewContext { get; set; }
         public PagingInfo PageModel { get; set; }
         public string PageAction { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             IUrlHelper urlHelper = _urlHelperFactory.GetUrlHelper(ViewContext);
-            TagBuilder result = new TagBuilder("div");
+
+            HtmlContentBuilder result = new HtmlContentBuilder();
+
+            TagBuilder tagLiPrevious = NextPreviousBuilder(PageModel.CurrentPage > 1, "Previous", "&laquo;");
+
+            result.AppendHtml(tagLiPrevious);
 
             for (int i = 1; i <= PageModel.TotalPages; i++)
             {
-                TagBuilder tag = new TagBuilder("a");
-                tag.Attributes["href"] = urlHelper.Action(PageAction, new {productPage = i});
-                tag.InnerHtml.Append(i.ToString());
-                result.InnerHtml.AppendHtml(tag);
+                TagBuilder tagLi = new TagBuilder("li");
+                tagLi.Attributes["class"] = "page-item";
+
+                if (PageModel.CurrentPage == i)
+                {
+                    tagLi.Attributes["class"] = "page-item active";
+                    TagBuilder tagSpan = new TagBuilder("span");
+                    tagSpan.Attributes["class"] = "page-link";
+                    tagSpan.InnerHtml.Append(i.ToString());
+
+                    TagBuilder tagSpanCurrent = new TagBuilder("span");
+                    tagSpanCurrent.Attributes["class"] = "sr-only";
+                    tagSpanCurrent.InnerHtml.Append("(current)");
+                    tagSpan.InnerHtml.AppendHtml(tagSpanCurrent);
+                    tagLi.InnerHtml.AppendHtml(tagSpan);
+                }
+                else
+                {
+                    TagBuilder tagA = new TagBuilder("a");
+                    tagA.Attributes["href"] = urlHelper.Action(PageAction, new {productPage = i});
+                    tagA.Attributes["class"] = "page-link";
+                    tagA.InnerHtml.Append(i.ToString());
+                    tagLi.InnerHtml.AppendHtml(tagA);
+                }
+
+
+                result.AppendHtml(tagLi);
             }
 
-            output.Content.AppendHtml(result.InnerHtml);
+            TagBuilder tagLiNext = NextPreviousBuilder(PageModel.CurrentPage < PageModel.TotalPages, "Next", "&raquo;");
+
+            result.AppendHtml(tagLiNext);
+
+            output.Content.AppendHtml(result);
+
+            TagBuilder NextPreviousBuilder(bool rule, string direction, string symbol)
+            {
+                TagBuilder tagLiNP = new TagBuilder("li");
+
+                tagLiNP.Attributes["class"] = rule ? "page-item" : "page-item disabled";
+
+                TagBuilder tagAprevious = new TagBuilder("a");
+
+                tagAprevious.Attributes["class"] = "page-link";
+                tagAprevious.Attributes["arial-label"] = direction;
+
+
+                tagAprevious.Attributes["href"] = rule
+                    ? urlHelper.Action(PageAction,
+                        new
+                        {
+                            productPage = direction == "Previous"
+                                ? PageModel.CurrentPage - 1
+                                : PageModel.CurrentPage + 1
+                        })
+                    : "";
+
+                TagBuilder tagSpanPrevious1 = new TagBuilder("span");
+                tagSpanPrevious1.Attributes["arial-hidden"] = "true";
+                tagSpanPrevious1.InnerHtml.AppendHtmlLine(symbol);
+
+                TagBuilder tagSpanPrevious2 = new TagBuilder("span");
+                tagSpanPrevious2.Attributes["class"] = "sr-only";
+                tagSpanPrevious2.InnerHtml.Append(direction);
+
+                tagAprevious.InnerHtml.AppendHtml(tagSpanPrevious1);
+                tagAprevious.InnerHtml.AppendHtml(tagSpanPrevious2);
+                tagLiNP.InnerHtml.AppendHtml(tagAprevious);
+
+                return tagLiNP;
+            }
         }
     }
 }
